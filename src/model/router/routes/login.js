c4ea3e023af7e31ee806e290d0c006ca715d02af
yt-theme <13133528465@163.com>
2019-07-05 18:32:25 +0800

    log

--- src/model/router/routes/login.js
+++ src/model/router/routes/login.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcrypt')
+
 const { Mongodb_model_user } = require('../../../db/mongodb/mongodb')
 
 module.exports = class {
@@ -11,14 +13,29 @@ module.exports = class {
             if (!req.body.username) {  res.json({ 'stat': 0, 'msg': '无 username' }) }
             // 否则进行正常程序
             else {
+                // 查询数据库
                 Mongodb_model_user().findOne({
                     'username': req.body.username,
                 }).then((v) => {
-                    res.json({
-                        'stat': 1,
-                        'msg': 'ok',
-                        'data': v['password']
-                    })
+
+                    // 如果检索出有此用户名则验证密码
+                    if (v['username'] === req.body.username) {
+                        // 验证密码
+                        if (bcrypt.compareSync(req.body.password, v['password'])) {
+                            res.json({
+                                'stat': 1,
+                                'msg': 'ok',
+                                'data': v['password']
+                            })
+                        } else {
+                            res.json({
+                                'stat': 0,
+                                'msg': 'err',
+                                'data': v['password']
+                            })
+                        }
+                    }
+                    
 
                 }).catch((err) => {
                     res.json({
