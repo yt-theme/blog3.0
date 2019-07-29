685a46e145b840f3a4cd7c23f609bfd39ce640ce
yt-theme <13133528465@163.com>
2019-07-29 10:44:55 +0800

    remove old project

--- frontEnd/BlogB-master/BlogB/mongodb.py
+++
@@ -1,7 +0,0 @@
-from mongoengine import *
-connect('blog')
-
-class User(Document):
-    username = StringField(required=True)
-    website  = URLField()
-    tags     = ListField(StringField(max_length=16))
--- frontEnd/BlogB-master/start.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-dir = "/home/a/pro/blog2.1beta/BlogB-master"
-python3 "$dir"manage.py runserver 0.0.0.0:8000
